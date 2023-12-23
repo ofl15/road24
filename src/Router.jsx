17f365f01c82e9d09c8195a0a4cc46a25b1175f7
ofl15 <olimovferuz880@gmail.com>
2023-12-23 20:42:29 +0500

    +

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { BrowserRouter, Routes , Route} from 'react-router-dom'
 import Main from './pages/Main'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import AddCar from './pages/AddCar'
+import SelectCar from './pages/SelectCar'
 
 const Router = () => {
   return (
     <BrowserRouter >
       <Routes>
         <Route exact path='/' Component={Main}/>
+        <Route exact path='/register' Component={Register}/>
+        <Route exact path='/login' Component={Login}/>
+        <Route exact path='/addcar' Component={AddCar}/>
+        <Route exact path='/selectcar' Component={SelectCar}/>
       </Routes>
     </BrowserRouter>
   )
