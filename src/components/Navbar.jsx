4c701a96a4f5db62b55be3571791b7d934f5aa11
ofl15 <olimovferuz880@gmail.com>
2024-01-24 19:23:42 +0500

    the end

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,7 @@ export default function Example() {
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <Link to="/card"
                   type="button"
-                  className="relative rounded-full mr-2 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 mr-8"
+                  className="relative rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 mr-8"
                 >
                   <span className="absolute -inset-1.5" />
                   <span className="sr-only">View notifications</span>
